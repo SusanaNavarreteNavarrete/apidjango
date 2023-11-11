e405380e261c1126fc4defd1d0cbea963f6a0053
SusanaNavarreteNavarrete <2019452094@teschi.edu.mx>
2023-11-10 18:39:55 -0600

    host

--- APIDJANGO9ISC22/settings.py
+++ APIDJANGO9ISC22/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i9))y^(jy#9&9$18+!9mdlsgw3z3&wqc0rl^ye^#_b$z^p3u$5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
