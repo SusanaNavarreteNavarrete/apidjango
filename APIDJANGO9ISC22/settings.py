d331672222f60c6cacfe0c165b71410f652d2e4b
SusanaNavarreteNavarrete <2019452094@teschi.edu.mx>
2023-12-07 22:46:44 -0600

    estilo

--- APIDJANGO9ISC22/settings.py
+++ APIDJANGO9ISC22/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['catalogo9isc22.onrender.com','*']
 INSTALLED_APPS = [
     'rest_framework',
     'api',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
