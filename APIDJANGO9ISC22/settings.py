8898baf706948f29d92eb409b76c9ece3e747a97
SusanaNavarreteNavarrete <2019452094@teschi.edu.mx>
2023-12-08 19:12:48 -0600

    stilos

--- APIDJANGO9ISC22/settings.py
+++ APIDJANGO9ISC22/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middlewere.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'APIDJANGO9ISC22.urls'
